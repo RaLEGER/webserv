374c1c6bbb78f5b00adfecfad977ffa7e210d98a
Raphael LEGER <rleger@bess-f2r2s8.clusters.42paris.fr>
2024-04-26 18:53:14 +0200

    file invalid

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: rleger <rleger@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 15:04:29 by rleger            #+#    #+#             */
-/*   Updated: 2024/04/26 12:28:23 by rleger           ###   ########.fr       */
+/*   Updated: 2024/04/26 18:52:27 by rleger           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,22 @@
 #include "ServerFarm.hpp"
 
 int main(int ac, char **av) {
+	std::vector <Server*> servers;
 	
 	signal(SIGINT, SIG_IGN);
 	if (ac != 2)
 		return 1;
     std::string filename = av[1];
-	Parser parser_obj(filename);
-	std::cout << "Parsed" << std::endl;
-	std::vector <Server*> servers;
-	servers = parser_obj.getServers();
+	if (filename.substr(std::max((int) filename.size() - 5, 0)).compare(".conf"))
+		return 1;
+	try {
+		Parser parser_obj(filename);
+		servers = parser_obj.getServers();
+	} catch (const CustomError& e) {
+		std::cerr << "Error parsing " << e.what() << std::endl;
+		return 1;
+	}
+
 	try {
 		ServerFarm serverfarm_obj(servers);
 		serverfarm_obj.set();
